73a26ddcb7920ce184bfe90fa68baa00315a9c85
Asaolu Elijah <asaoluelijah7@gmail.com>
2022-06-24 19:53:25 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      	This is for my 
+        <a
+          className="App-link"
+          href="https://github.com/zafar-saleem/github-actions-heroku"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          blog post tutorial
+        </a>
+         on setting up github actions with heroku for CI/CD.
+        <button>Does nothing</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
